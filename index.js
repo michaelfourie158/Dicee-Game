677b2fa40abf88914a1f65476529d2b92121e9bb
Michael Fourie <michaelfourie115588@gmail.com>
2024-09-03 21:08:23 +0200

    changed title and some formatting

--- index.js
+++ index.js
@@ -4,13 +4,14 @@ console.log(randomNumber1);
 var randomDiceImage1 = "dice" + randomNumber1 + ".png";
 var randomImageSource1 = "/images/" + randomDiceImage1;
 
+document.querySelectorAll("img")[0].setAttribute("src", randomImageSource1);
+
 let randomNumber2 = Math.floor(Math.random() * 6) + 1;
 console.log(randomNumber2);
 
 let randomDiceImage2 = "dice" + randomNumber2 + ".png";
 let randomImageSource2 = "/images/" + randomDiceImage2;
 
-document.querySelectorAll("img")[0].setAttribute("src", randomImageSource1);
 document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
 
 if (randomNumber1 > randomNumber2) {
